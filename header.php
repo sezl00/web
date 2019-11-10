473be5af75dbaf564cd1fac95844a6db1ffb2535
sezl00 <47723094+sezl00@users.noreply.github.com>
2019-11-10 23:01:33 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,43 @@
+﻿<!-- HLAVICKA: -->
+<header class="header <br />
+        <b>Notice</b>:  Undefined variable: header_widthbg in <b>/Applications/MAMP/htdocs/lbcc-investment/dev/detail-sluzby.php</b> on line <b>7</b><br />
+        " id="js-header">
+
+    <div class="container container--large">
+
+        <!-- logo / citat -->
+        <div class="header__left">
+            <a href="index.php" class="header__logo">
+                            </a>
+
+            <img class="citate" src="images/citate.png" alt="">
+        </div>
+
+        <!-- hamburger opened menu / prihlaseni -->
+        <div class="header__right">
+
+            <!-- tlacitko otevirajici menu od 959px -->
+            <button class="hamburger" data-class="active" data-target="#js-menu" data-close-onblur="true" onclick="toggle(event, this);openNav()" data-body-freeze="true" data-effect="fade">
+                <span class="hamburger__text">Menu</span>
+                <svg><use xlink:href="#icon-menu"/></svg>
+            </button>
+
+  
+            </a>
+        </div>
+    </div>
+
+    <div class="menu" id="js-menu">
+        <aside class="menu__in">
+            <div class="menu__links">
+                <a href="index.php">Úvod</a>
+                <a href="index.php#js-fonds">Dluhopisy</a>
+                <a href="index.php#investicni-projekty">Naše investiční projekty</a>
+                <a href="index.php#o-nas">O nás</a>
+                <a href="index.php#sponzorujeme">Sponzorujeme</a>
+                <a href="index.php#tym">Náš tým</a>
+                <a href="index.php#kontakt">Kontakt</a>
+            </div>
+        </aside>
+    </div>
+</header>
