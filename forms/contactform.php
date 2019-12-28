c0913087c1ef1556f7cc71c92d5ab8f928b86adf
sezl00 <47723094+sezl00@users.noreply.github.com>
2019-12-28 09:51:04 +0100

    Add files via upload

---
+++ forms/contactform.php
@@ -0,0 +1,26 @@
+﻿<?php
+	if($_POST['email']=='' || !(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))){
+		echo 'ko';
+	}elseif($_POST['name']==''){
+		echo 'ko';	
+	}elseif($_POST['message']==''){
+		echo 'ko';	
+	}else{
+		$subject = "=?utf-8?B?".base64_encode("Kontaktní formulář ze stránek LBCC investment")."?=";
+
+		$txt = 	"Jmeno: ".$_POST['name'].
+				"\n\nE-mail: ".$_POST['email'].
+				"\n\nPožadavek: ".$_POST['option'].				"\n\nZpráva: ".$_POST['message']."\n\n";
+
+		//$headers = "From: ".$_POST['email'];
+		$headers = "MIME-Version: 1.0\n";
+        $headers .= "Content-Type: text/plain; charset=utf-8\n";
+        $headers .= "From: =?UTF-8?B?".base64_encode($_POST['name'])."?=<".$_POST['email'].">\n";
+		
+        if(mail ("info@lbcc.cz", $subject, $txt, $headers)){
+			echo 'ok';
+		}else{
+			echo 'ko';	
+		}
+	}
+?>
